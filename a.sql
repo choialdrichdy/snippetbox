40d5a70a52819aede587642c6f51a30e08c8c188
Aldrich Choi <choi.aldrich@gmail.com>
2023-12-19 18:17:41 +0900

    feat: Adds code in chapter 8 (#1)

--- a.sql
+++ a.sql
@@ -1,10 +1,21 @@
+CREATE TABLE snippets (
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    title VARCHAR(100) NOT NULL,
+    content TEXT NOT NULL,
+    created DATETIME NOT NULL,
+    expires DATETIME NOT NULL
+);
+
+-- Add an index on the created column.
+CREATE INDEX idx_snippets_created ON snippets(created);
+
 INSERT INTO snippets (title, content, created, expires) VALUES (
     'An old silent pond',
     'An old silent pond...
-    A frog jumps into the pond,
-    splash! Silence again.
-    
-    – Matsuo Bashō',
+A frog jumps into the pond,
+splash! Silence again.
+
+– Matsuo Bashō',
     DATETIME('now'),
     DATETIME(DATETIME('now'), '+1 year')
 );
@@ -12,10 +23,10 @@ INSERT INTO snippets (title, content, created, expires) VALUES (
 INSERT INTO snippets (title, content, created, expires) VALUES (
     'Over the wintry forest',
     'Over the wintry
-    forest, winds howl in rage
-    with no leaves to blow.
-    
-    – Natsume Soseki',
+forest, winds howl in rage
+with no leaves to blow.
+
+– Natsume Soseki',
     DATETIME('now'),
     DATETIME(DATETIME('now'), '+1 year')
 );
@@ -23,10 +34,10 @@ INSERT INTO snippets (title, content, created, expires) VALUES (
 INSERT INTO snippets (title, content, created, expires) VALUES (
     'First autumn morning',
     'First autumn morning
-    the mirror I stare into
-    shows my father''s face.
-    
-    – Murakami Kijo',
+the mirror I stare into
+shows my father''s face.
+
+– Murakami Kijo',
     DATETIME('now'),
     DATETIME(DATETIME('now'), '+7 days')
 );
--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -6,14 +6,12 @@ import (
 	"net/http"
 	"strconv"
 
+	"github.com/julienschmidt/httprouter"
 	"snippetbox.aldrich.com/internal/models"
+	"snippetbox.aldrich.com/internal/validator"
 )
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		http.NotFound(w, r)
-		return
-	}
 
 	snippets, err := app.snippets.Latest()
 	if err != nil {
@@ -27,7 +25,8 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
-	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	params := httprouter.ParamsFromContext(r.Context())
+	id, err := strconv.Atoi(params.ByName("id"))
 	if err != nil || id < 1 {
 		// http.NotFound(w, r)
 		app.notFound(w)
@@ -50,23 +49,54 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		w.Header().Set("Allow", "POST")
-		// http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
-		app.clientError(w, http.StatusMethodNotAllowed)
+	data := app.newTemplateData(r)
+	data.Form = snippetCreateForm{
+		Expires: "+365 days",
+	}
+	app.render(w, r, http.StatusOK, "create.tmpl", data)
+}
+
+type snippetCreateForm struct {
+	Title   string
+	Content string
+	Expires string
+	validator.Validator
+}
+
+func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
+
+	err := r.ParseForm()
+	if err != nil {
+		app.clientError(w, http.StatusBadRequest)
 		return
 	}
 
-	title := "0 snail"
-	content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n– Kobayashi Issa"
-	expires := "+7 days"
+	form := snippetCreateForm{
+		Title:   r.PostForm.Get("title"),
+		Content: r.PostForm.Get("content"),
+		Expires: r.PostForm.Get("expires"),
+	}
 
-	id, err := app.snippets.Insert(title, content, expires)
+	// fieldErrors := make(map[string]string)
+
+	form.CheckField(validator.NotBlank(form.Title), "title", "This field cannot be blank")
+	form.CheckField(validator.MaxChars(form.Title, 100), "title", "This field cannot be more than 100 characters long")
+	form.CheckField(validator.NotBlank(form.Content), "content", "This field cannot be blank")
+	form.CheckField(validator.PermittedValue(form.Expires, "+1 days", "+7 days", "+365 days"), "expires", "This field must equal 1, 7 or 365")
+
+	if !form.Valid() {
+		data := app.newTemplateData(r)
+		data.Form = form
+		app.render(w, r, http.StatusUnprocessableEntity, "create.tmpl", data)
+		return
+	}
+
+	id, err := app.snippets.Insert(form.Title, form.Content, form.Expires)
 	if err != nil {
 		app.serverError(w, r, err)
 		return
 	}
 
-	http.Redirect(w, r, fmt.Sprintf("/snippet/view?id=%d", id), http.StatusSeeOther)
+	http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
 	// w.Write([]byte("Create a new snippet...\n"))
 }
